b60bd10227dd93b8321fa9a83c9178d978438616
Haley May <haymon24u@gmail.com>
2022-09-29 15:35:03 -1000

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,161 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is the project title?",
+    name: "title",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "What is a short description of your project?",
+    name: "description",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "How to you intall this app?",
+    name: "installation",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "What are instuctions and examples for use?",
+    name: "usage",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "List any collaborators",
+    name: "credits",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+  {
+    type: "list",
+    message: "What license did you use?",
+    name: "license",
+    choices: [
+      "Apache 2.0",
+      "EPL 1.0",
+      "BSD 2",
+      "GNU v3",
+      "MIT",
+      "Mozilla",
+      "None of the Above",
+    ],
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "How can others contribute?",
+    name: "contributions",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "Do you have test examples for this application?",
+    name: "testing",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "Enter your GitHub username",
+    name: "github",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "Enter your Email",
+    name: "email",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "i need a value to continue";
+      }
+    },
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    console.log(fileName);
+    console.log(data);
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("sucess");
+    }
+  });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then(function (data) {
+    writeToFile("README.md", generateMarkdown(data));
+    console.log(data);
+  });
+}
+
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,101 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  let badge = "";
+  if (license === "Apache 2.0") {
+    badge =
+      "![License Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+  } else if (license === "EPL 1.0") {
+    badge =
+      "![License EPL](https://img.shields.io/badge/License-EPL%201.0-red.svg)";
+  } else if (license === "BSD 2") {
+    badge =
+      "![License BSD 2](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)";
+  } else if (license === "GNU v3") {
+    badge =
+      "![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)";
+  } else if (license === "MIT") {
+    badge =
+      "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  } else if (license === "Mozilla") {
+    badge =
+      "![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)";
+  } else {
+    badge = "";
+  }
+  return badge;
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  let licenseLink = "";
+  if (license === "Apache 2.0") {
+    licenseLink = "https://www.apache.org/licenses/LICENSE-2.0";
+  } else if (license === "EPL 1.0") {
+    licenseLink = "https://www.eclipse.org/legal/epl-v10.html";
+  } else if (license === "BSD 2") {
+    licenseLink = "https://opensource.org/licenses/BSD-2-Clause";
+  } else if (license === "GNU v3") {
+    licenseLink = "https://www.gnu.org/licenses/gpl-3.0.en.html";
+  } else if (license === "MIT") {
+    licenseLink = "https://choosealicense.com/licenses/mit/";
+  } else if (license === "Mozilla ") {
+    licenseLink = "https://www.mozilla.org/en-US/MPL/";
+  } else {
+    licenseLink = "";
+  }
+  return licenseLink;
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  let licenseSection = "";
+  if (license === "None of the Above") {
+    licenseSection = "";
+  } else {
+    licenseSection = `License: ${license}`;
+  }
+  return licenseSection;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## ${renderLicenseSection(data.license)} ${renderLicenseBadge(data.license)}
+### ${renderLicenseLink(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [Contributions](#contributions)
+* [Testing](#testing)
+* [Questions](#questions)
+
+## Installation:
+${data.installation}
+
+## Usage:
+${data.usage}
+
+## Credits:
+${data.credits}
+
+## Contributions:
+${data.contributions}
+
+## Testing:
+${data.testing}
+
+## Questions:
+ - Github: [${data.github}](https://github.com/${data.github})
+ - Email: [${data.email}]`;
+}
+
+module.exports = generateMarkdown;
